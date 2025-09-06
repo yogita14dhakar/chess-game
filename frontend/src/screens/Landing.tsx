dee9125716da27642f8710d41b85094d84301dc4
yogita dhakar <yogitadhakar5@gmail.com>
2025-09-06 15:56:47 +0530

    worked on orientation

--- frontend/src/screens/Landing.tsx
+++ frontend/src/screens/Landing.tsx
@@ -3,6 +3,7 @@ import { Button } from "../components/Button";
 import { BACKEND_URL } from "../atoms/user";
 import { useUser } from "../hooks/useUser";
 import { v4 as uuidv4 } from 'uuid';
+import { useEffect, useState } from "react";
 
 const gameId = uuidv4();
 function getUrl(){
@@ -10,18 +11,19 @@ function getUrl(){
     return user;
 }
 
+const [isLandscape, setIsLandscape] = useState(screen.orientation.type === 'landscape-primary' || screen.orientation.type === 'landscape-secondary');
+
+useEffect(() => {
+        setIsLandscape(screen.orientation.type === 'landscape-primary' || screen.orientation.type === 'landscape-secondary');
+}, [screen.orientation]);
+
 export function Landing(){
     const navigate = useNavigate();
     return (<div className="flex justify-center pt-20">
         <div className="max-w-screen-lg max-h-screen-lg">
         <h1 className="md:text-6xl text-4xl font-bold text-white text-center">Let's Play Chess Together</h1>
             <div className="flex justify-center gap-4">
-                {screen.orientation.type === 'landscape-primary' || screen.orientation.type === 'landscape-secondary' ? 
-                    (<div className="flex justify-center mt-5 md:mt-20 ms:hidden">
-                        <img src="chessImage.png" className="h-96 "></img>
-                    </div>)
-                    :''
-                }            
+                {isLandscape ? (<div className="flex justify-center mt-5 md:mt-20"><img src="chessImage.png" className="h-96 "></img></div>) :''}            
                 <div className="flex flex-col justify-center mt-20 gap-4">
                         {getUrl() == null ? <Button onClick={ () => navigate(`/login`)} content="Login"></Button>:
                         <Button onClick={ () => window.open(`${BACKEND_URL}/auth/logout`, '_self')} content="logout"/>}
