3e1bc56513765f994ad65a1e507b5ea39964ac28
yogita dhakar <yogitadhakar5@gmail.com>
2025-09-07 15:46:33 +0530

    changed isLandscape

--- frontend/src/screens/Landing.tsx
+++ frontend/src/screens/Landing.tsx
@@ -11,10 +11,10 @@ function getUrl(){
     return user;
 }
 const getOrientation = () => window.screen.orientation.type;
-const [isLandscape, setIsLandscape] = useState(getOrientation() === 'landscape-primary' || getOrientation() === 'landscape-secondary');
+const [isLandscape, setIsLandscape] = useState(getOrientation());
 
 screen.orientation.onchange = function() {
-  setIsLandscape(getOrientation() === 'landscape-primary' || getOrientation() === 'landscape-secondary');
+  setIsLandscape(getOrientation());
 };
 
 export function Landing(){
@@ -23,7 +23,7 @@ export function Landing(){
         <div className="max-w-screen-lg max-h-screen-lg">
         <h1 className="md:text-6xl text-4xl font-bold text-white text-center">Let's Play Chess Together</h1>
             <div className="flex justify-center gap-4">
-                {isLandscape ? (<div className="flex justify-center mt-5 md:mt-20"><img src="chessImage.png" className="h-96 "></img></div>) :''}            
+                {isLandscape === 'landscape-primary' || isLandscape === 'landscape-secondary' ? (<div className="flex justify-center mt-5 md:mt-20"><img src="chessImage.png" className="h-96 "></img></div>) :''}            
                 <div className="flex flex-col justify-center mt-20 gap-4">
                         {getUrl() == null ? <Button onClick={ () => navigate(`/login`)} content="Login"></Button>:
                         <Button onClick={ () => window.open(`${BACKEND_URL}/auth/logout`, '_self')} content="logout"/>}
