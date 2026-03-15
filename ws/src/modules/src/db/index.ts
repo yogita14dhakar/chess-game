f49bc67affa5596f6b46b8496394cb99547f3142
Yogita Dhakar <yogitadhakar5@gmail.com>
2026-03-15 20:13:53 +0530

    Update index.ts

--- ws/src/modules/src/db/index.ts
+++ ws/src/modules/src/db/index.ts
@@ -45,7 +45,7 @@ export const update = async (q: string) => {
         // connection = await connPool.getConnection();
         const [rows, err]: [mysql.RowDataPacket[], FieldPacket[]] = await connPool.query(q);
             if(err) throw err;
-            return JSON.parse(JSON.stringify(rows[0]));
+            return JSON.parse(JSON.stringify(rows));
     }catch(err){
         console.log(err);
     }
