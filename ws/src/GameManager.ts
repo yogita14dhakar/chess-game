bcfb50bd462cd82047dfc1a2275d04e1197dda92
Yogita Dhakar <yogitadhakar5@gmail.com>
2026-03-15 18:10:13 +0530

    Update GameManager.ts

--- ws/src/GameManager.ts
+++ ws/src/GameManager.ts
@@ -147,7 +147,7 @@ export class GameManager{
                 }
         
                 let availableGame = this.games.find((game) => game.gameId === gameId);
-                const gameFromDb = await update(`SELECT * FROM Game WHERE id = '${gameId}'`); 
+                const gameFromDb = await findMany(`SELECT * FROM Game WHERE id = '${gameId}'`); 
                 console.log(gameId, gameFromDb);
                 const whitePlayer = await update(`SELECT * FROM User WHERE id = '${gameFromDb.whitePlayerId}'`);
                 const blackPlayer = await update(`SELECT * FROM User WHERE id = '${gameFromDb.blackPlayerId}'`);
