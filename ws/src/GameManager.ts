8d45a416a6df4d3c8054f78cdd876dff0a27d9c0
Yogita Dhakar <yogitadhakar5@gmail.com>
2026-03-15 20:25:15 +0530

    Update GameManager.ts

--- ws/src/GameManager.ts
+++ ws/src/GameManager.ts
@@ -147,13 +147,10 @@ export class GameManager{
                 }
         
                 let availableGame = this.games.find((game) => game.gameId === gameId);
-                console.log(update);
                 const gameFromDb = await update(`SELECT * FROM Game WHERE id = '${gameId}'`); 
-                console.log(gameFromDb);
                 const whitePlayer = await findMany(`SELECT * FROM User WHERE id = '${gameFromDb.whitePlayerId}'`);
                 const blackPlayer = await findMany(`SELECT * FROM User WHERE id = '${gameFromDb.blackPlayerId}'`);
                 const moves = await findMany(`SELECT * FROM Move WHERE gameId = '${gameFromDb?.id}' ORDER BY moveNumber ASC`);
-                console.log(gameFromDb, whitePlayer, blackPlayer);
                 // There is a game created but no second player available
         
                 if (availableGame && !availableGame.player2UserId) {
@@ -173,7 +170,6 @@ export class GameManager{
                   }
 
                   if(gameFromDb.status !== GameStatus.IN_PROGRESS) {
-                      console.log('ending game');
                     user.socket.send(JSON.stringify({
                       type: GAME_ENDED,
                       payload: {
